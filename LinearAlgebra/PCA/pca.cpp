c944096f3ab508bccdb8caf0d4063decd7ef99dc
Tungyen <cony998820@gmail.com>
2024-01-22 14:01:23 -0800

    Remove redundant files

---
+++ LinearAlgebra/PCA/pca.cpp
@@ -0,0 +1,28 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace Eigen;
+
+class PCA{
+
+public:
+    PCA(){
+
+    }
+
+    MatrixXf compress(MatrixXf A, int k){
+        MatrixXf result;
+        return result;
+    }
+
+private:
+
+};
+
+int main()
+{
+
+    return 0;
+}
