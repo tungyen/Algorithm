4d5ef4758c6cdd353be8219d15345f79e212f7e2
Tungyen <cony998820@gmail.com>
2024-02-07 21:50:25 -0800

    Finish PCA

--- LinearAlgebra/PCA/pca.cpp
+++ LinearAlgebra/PCA/pca.cpp
@@ -8,21 +8,47 @@ using namespace Eigen;
 class PCA{
 
 public:
-    PCA(){
-
+    PCA(int k){
+        K = k;
     }
 
-    MatrixXf compress(MatrixXf A, int k){
-        MatrixXf result;
-        return result;
+    void ProjReconstruct(MatrixXf data){
+
+        int d = data.cols();
+
+        VectorXf mean = data.colwise().mean();
+        MatrixXf diff = data.colwise() - mean;
+        MatrixXf conv = diff.transpose() * diff;
+
+        JacobiSVD<MatrixXf> svd(conv, ComputeFullU | ComputeFullV);
+        MatrixXf U = svd.matrixU();
+        MatrixXf ProjectionMatrix = U.block(0, 0, d, K);
+
+        Projection = data * ProjectionMatrix;
+        Reconstruction = Projection * ProjectionMatrix.transpose();
+    }
+
+    MatrixXf getProjection(){
+        return Projection;
+    }
+
+    MatrixXf getReconstruction(){
+        return Reconstruction;
     }
 
 private:
-
+    int K;
+    MatrixXf Projection;
+    MatrixXf Reconstruction;
 };
 
 int main()
 {
+    MatrixXf data = Eigen::MatrixXf::Random(100, 8);
+    PCA pca = PCA(3);
+    pca.ProjReconstruct(data);
+    cout<<"row of Projection: "<<pca.getProjection().rows()<<", col of Projection: "<<pca.getProjection().cols()<<endl;
+    cout<<"row of Reconstruction: "<<pca.getReconstruction().rows()<<", col of Reconstruction: "<<pca.getReconstruction().cols()<<endl;
 
     return 0;
 }
